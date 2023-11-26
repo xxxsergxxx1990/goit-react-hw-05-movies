2a1d42662524a5585e612fa7e66bd1ade6916e7d
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-11-26 13:47:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import { App } from 'App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename = 'goit-react-hw-05-movies'>
+  <React.StrictMode>
+  <BrowserRouter basename = '/goit-react-hw-05-movies'>
     <App />
   </BrowserRouter>
+  </React.StrictMode>
 );
