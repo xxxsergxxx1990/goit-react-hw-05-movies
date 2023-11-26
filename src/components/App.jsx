4cb90d916a30bca46e366633afe79facd7bda0f3
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-11-26 14:05:17 +0200

    /////////

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,9 @@
-import React, { lazy, Suspense } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-
-import './App.css';
-
-const Layout = lazy(() => import('components/Layout/Layout'));
-const Home = lazy(() => import('pages/Home'));
-const Movies = lazy(() => import('pages/Movies'));
-const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
-const Cast = lazy(() => import('components/Cast/Cast'));
-const Reviews = lazy(() => import('components/Reviews'));
-
-const ErrorBoundary = () => {
-  return <h3>Wystąpił błąd podczas ładowania komponentu.</h3>;
-};
-
+import { Routes,Route } from "react-router-dom";
+import {Cast} from './Cast/Cast'
 export const App = () => {
   return (
-    <Suspense fallback={<ErrorBoundary />}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-      </Routes>
-    </Suspense>
+  <>
+  <Cast/>
+  </>
   );
 };
