0c801a0cf5c04b6ffe5631a386846bbe881ebb45
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-11-26 16:09:02 +0200

    //

--- src/Api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-const BASE_URL = 'https://api.themoviedb.org/3'
-const KEY = '2e9dbb0fd6fdd1db95c5775e25b4f2ab'
-
-
--- src/services/getMovies.js
+++ src/services/getMovies.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
-const API_KEY = '3373af60a4ee1fe7510a1a61c11380e1';
+const API_KEY = '2e9dbb0fd6fdd1db95c5775e25b4f2ab';
 
 export const getTrendingMovies = async () => {
   const { data } = await axios.get(`trending/movie/week?api_key=${API_KEY}`);
