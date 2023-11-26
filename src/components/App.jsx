3f7742041bde9eb597a6641ebc1a854aab230122
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-11-26 15:11:06 +0200

    zz

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,16 @@
-import React, { lazy, Suspense } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-
-const Layout = lazy(() => import('components/Layout/Layout'));
-const Home = lazy(() => import('Pages/Home'));
-const Movies = lazy(() => import('Pages/Movies'));
-const MovieDetails = lazy(() => import('Pages/MovieDetails/MovieDetails'));
-const Cast = lazy(() => import('components/Cast/Cast'));
-const Reviews = lazy(() => import('components/Reviews'));
 export const App = () => {
   return (
-    <Suspense fallback="Please wait.">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
